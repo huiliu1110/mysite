303ccb3587f41493b0cfe7f07e9550eb1317fbb4
haleyhuiliu <hui@primer.ai>
2019-12-06 22:16:28 -0800

    add i18n

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { useTranslation } from 'react-i18next';
 import logo from './logo.svg';
 import './App.css';
+
+function LanguageSelector() {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (event) => {
+    i18n.changeLanguage(event.target.value)
+  }
+
+  return (
+    <div onChange={changeLanguage}>
+      <input type="radio" value="en" name="language" defaultChecked /> {t('language-en.label')}
+      <input type="radio" value="zh" name="language"/> {t('language-zh.label')}
+    </div>
+  )
+}
+
+function MyComponent() {
+  const { t, i18n } = useTranslation();
+
+  return <h1>{t('name.label')}</h1>
+}
 
 function App() {
   return (
@@ -19,6 +41,10 @@ function App() {
           Learn React
         </a>
       </header>
+      <Suspense fallback="loading">
+        <LanguageSelector />
+        <MyComponent />
+      </Suspense>
     </div>
   );
 }
