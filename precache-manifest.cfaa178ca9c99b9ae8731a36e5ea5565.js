97a0d13a02ac90f65fd6eb5bb6628575e33452b9
haleyhuiliu <hui@primer.ai>
2019-12-07 02:51:11 -0800

    Updates

---
+++ precache-manifest.cfaa178ca9c99b9ae8731a36e5ea5565.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b873995717460e1fc2b1e21c5facf2e",
+    "url": "/mysite/index.html"
+  },
+  {
+    "revision": "458c00c71e31fb320c71",
+    "url": "/mysite/static/css/main.88824ec2.chunk.css"
+  },
+  {
+    "revision": "02a657b9dfdec82d8dbe",
+    "url": "/mysite/static/js/2.21a712c1.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/mysite/static/js/2.21a712c1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "458c00c71e31fb320c71",
+    "url": "/mysite/static/js/main.1d4e6e1d.chunk.js"
+  },
+  {
+    "revision": "ffa961f0cbf238980333",
+    "url": "/mysite/static/js/runtime-main.277d5d72.js"
+  }
+]);
